d63c5135bf22b92011b329d66cab16f599387fa5
Sookie <taek171@daum.net>
2016-11-08 22:08:03 +0900

    Chapter 6

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,5 +1,6 @@
 class Ticket < ApplicationRecord
   belongs_to :project
+  belongs_to :author, class_name: "User"
   validates :name, :description, presence: true
   validates :description, length: { maximum: 1000 }
   validates :description, length: { minimum: 10 }
